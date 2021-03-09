8a5bcc9757b08644c470c479bba565f99f93eac7
Pham Minh Quang <qquang24t5@gmail.com>
2021-03-09 07:37:25 +0700

    Tích hợp JUnit 5

---
+++ src/test/java/com/github/qquang24t5/_8tea/EightTeaApplicationTest.java
@@ -0,0 +1,14 @@
+package com.github.qquang24t5._8tea;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class EightTeaApplicationTest {
+
+    @Test
+    void add1To1ShouldReturn2() {
+        assertEquals(2, 1 + 1);
+    }
+
+}

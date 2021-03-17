81633c4f7e40ff73ef6f720663d76a69690c69a1
Pham Minh Quang <qquang24t5@gmail.com>
2021-03-17 13:16:18 +0700

    Tạo khung cho các bước Given của Employee

---
+++ src/test/java/com/github/qquang24t5/_8tea/it/employee/EmployeeGivenStepDefinitions.java
@@ -0,0 +1,30 @@
+package com.github.qquang24t5._8tea.it.employee;
+
+import io.cucumber.java.en.Given;
+
+public class EmployeeGivenStepDefinitions {
+
+    @Given("the account with the phone number {string} doesn't exist in the system")
+    public void the_account_with_the_phone_number_doesn_t_exist_in_the_system(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Given("the account with the email {string} doesn't exist in the system")
+    public void the_account_with_the_email_doesn_t_exist_in_the_system(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Given("the account with the phone number {string} already exists in the system")
+    public void the_account_with_the_phone_number_already_exists_in_the_system(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Given("the account with the email {string} already exists in the system")
+    public void the_account_with_the_email_already_exists_in_the_system(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+}

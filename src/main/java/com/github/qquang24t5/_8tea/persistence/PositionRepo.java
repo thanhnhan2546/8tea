2afae44952f904f7af1c9b25766a8a627b060938
Pham Minh Quang <qquang24t5@gmail.com>
2021-03-26 15:10:58 +0700

    PositionRepo: thêm javadoc cho, sửa lại hàm getById
    - Hàm getById khi không có dữ liệu trong bảng, quăng IndexOutOfBoundException, giờ sẽ trả về null

--- src/main/java/com/github/qquang24t5/_8tea/persistence/PositionRepo.java
+++ src/main/java/com/github/qquang24t5/_8tea/persistence/PositionRepo.java
@@ -3,15 +3,17 @@ package com.github.qquang24t5._8tea.persistence;
 import com.github.qquang24t5._8tea.transference.Position;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
-import org.hibernate.query.Query;
 
 import javax.persistence.TypedQuery;
 import java.util.List;
 
 public class PositionRepo extends Repo {
 
+    /**
+     * Truy vấn tất cả {@code Position}
+     * @return danh sách tất cả các {@code Position}, danh sách rỗng nếu như bảng không có dữ liệu
+     */
     public List<Position> getAll() {
-
         Session s = database.openSession();
         Transaction t = s.beginTransaction();
         TypedQuery<Position> q = s.createQuery("FROM Position", Position.class);
@@ -21,15 +23,26 @@ public class PositionRepo extends Repo {
         return p;
     }
 
+    /**
+     * Truy vấn {@code Position} bằng {@code id}
+     * @param id id truy vấn
+     * @return {@code Position} với id đã cho, null nếu id đã cho không tồn tại
+     */
     public Position getById(Integer id) {
         Session s = database.openSession();
         Transaction t = s.beginTransaction();
         TypedQuery<Position> q = s.createQuery("FROM Position WHERE id = :id", Position.class);
         q.setParameter("id", id);
-        Position p = (Position) q.getResultList().get(0);
+        List<Position> p = q.getResultList();
         t.commit();
         s.close();
-        return p;
+
+        if (p.isEmpty()) {
+            return null;
+        } else {
+            return p.get(0);
+        }
+
     }
 
 }
